7c2ea25707a5d2788ebbfc48377516a0e4826d99
Kenneth Jones <kendjone@iu.edu>
2020-02-28 14:53:28 -0600

    Update package name

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kennethjones/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kennethjones/Code/flutter-plugins/packages/local_auth/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/kennethjones/Code/local_auth_device_credentials/example"
+export "FLUTTER_TARGET=/Users/kennethjones/Code/local_auth_device_credentials/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/kennethjones/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
